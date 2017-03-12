7cf3d235c22a7a07c252ec066391d67f65041580
jeaye <contact@jeaye.com>
2017-03-12 12:27:12 -0700

    Refactor lang bits into separate ns

--- Assets/polyvore/player/input.clj
+++ Assets/polyvore/player/input.clj
@@ -5,22 +5,8 @@
             Time Physics])
   (:require [arcadia
              [core :refer :all]
-             [linear :refer :all]]))
-
-(defmacro update! [field fun]
-  `(set! ~field (~fun ~field)))
-
-(defmacro case-enum
-  "Like `case`, but explicitly dispatch on native enum ordinals."
-  [e & clauses]
-  (letfn [(enum-ordinal [e] `(let [^Enum e# ~e] (int e#)))]
-    `(case ~(enum-ordinal e)
-       ~@(concat
-           (mapcat (fn [[test result]]
-                     [(eval (enum-ordinal test)) result])
-                   (partition 2 clauses))
-           (when (odd? (count clauses))
-             (list (last clauses)))))))
+             [linear :refer :all]]
+            [polyvore.util.lang :refer :all]))
 
 (def inc-float (comp float inc))
 (def dec-float (comp float dec))
---
+++ Assets/polyvore/util/lang.clj
@@ -0,0 +1,16 @@
+(ns polyvore.util.lang)
+
+(defmacro update! [field fun]
+  `(set! ~field (~fun ~field)))
+
+(defmacro case-enum
+  "Like `case`, but explicitly dispatch on native enum ordinals."
+  [e & clauses]
+  (letfn [(enum-ordinal [e] `(let [^Enum e# ~e] (int e#)))]
+    `(case ~(enum-ordinal e)
+       ~@(concat
+           (mapcat (fn [[test result]]
+                     [(eval (enum-ordinal test)) result])
+                   (partition 2 clauses))
+           (when (odd? (count clauses))
+             (list (last clauses)))))))
