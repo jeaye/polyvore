923936c3c9aeda5e71e8c59af1f1536c48fa3d6b
jeaye <contact@jeaye.com>
2017-03-11 21:52:24 -0800

    Add basic third person movement

--- Assets/polyvore/player/input.clj
+++ Assets/polyvore/player/input.clj
@@ -1,6 +1,29 @@
 (ns polyvore.player.input
-  (:import [UnityEngine])
-  (:require [arcadia.core :refer :all]))
+  (:import [UnityEngine Input KeyCode Vector3 Time CharacterController])
+  (:require [arcadia
+             [core :refer :all]
+             [linear :refer :all]]))
 
-(defn meow []
-  (log "meow"))
+(defmacro update! [field fun]
+  `(set! ~field (~fun ~field)))
+
+(def inc-float (comp float inc))
+(def dec-float (comp float dec))
+
+(def speed 1.0)
+
+(defn move! [obj]
+  (let [pressed (->> (mapv #(vector % (Input/GetKey %))
+                           [KeyCode/W KeyCode/A KeyCode/S KeyCode/D])
+                     (filter #(true? (second %)))
+                     (into {}))
+        diff (reduce (fn [acc v]
+                       (condp = (first v)
+                         KeyCode/W (do (update! (.z acc) inc-float) acc)
+                         KeyCode/S (do (update! (.z acc) dec-float) acc)
+                         KeyCode/A (do (update! (.x acc) dec-float) acc)
+                         KeyCode/D (do (update! (.x acc) inc-float) acc)))
+                     (Vector3.)
+                     pressed)
+        adjusted-diff (v3* (v3* diff speed) Time/deltaTime)]
+    (.. (cmpt obj UnityEngine.CharacterController) (Move adjusted-diff))))
