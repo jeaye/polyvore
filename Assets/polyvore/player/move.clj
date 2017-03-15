63c7e1e37c46b4c770c34dc3bec3ca0cce4e3d98
jeaye <contact@jeaye.com>
2017-03-14 22:35:09 -0700

    Add state to player move hook

--- Assets/polyvore/player/move.clj
+++ Assets/polyvore/player/move.clj
@@ -1,4 +1,4 @@
-(ns polyvore.player.input
+(ns polyvore.player.move
   (:import [UnityEngine
             GameObject
             Input KeyCode
@@ -10,8 +10,10 @@
              [linear :refer :all]]
             [polyvore.util.lang :refer :all]))
 
-(def speed 1.0)
-(def jump-height 20.0)
+(defn init-state! [^GameObject obj]
+  (doto obj
+    (set-state! ::speed 1.0)
+    (set-state! ::jump-height 20.0)))
 
 (defn get-inputs! []
   (->> (mapv #(vector % (Input/GetKey %))
@@ -19,22 +21,25 @@
        (filter #(true? (second %)))
        (into {})))
 
-(defn move [inputs]
+(defn calculate-diff [obj-state inputs]
   (let [diff (reduce (fn [acc v]
                        (case-enum (first v)
                          KeyCode/W (do (update! (.z acc) inc-float) acc)
                          KeyCode/S (do (update! (.z acc) dec-float) acc)
                          KeyCode/A (do (update! (.x acc) dec-float) acc)
                          KeyCode/D (do (update! (.x acc) inc-float) acc)
-                         KeyCode/Space (do (update! (.y acc)
-                                                    #(float (+ jump-height %)))
+                         KeyCode/Space (do
+                                         (update! (.y acc)
+                                                  #(-> (::jump-height obj-state)
+                                                       (+ %)
+                                                       float))
                                            acc)))
-                     (v3 0.0 (.y Physics/gravity) 0.0)
+                     (v3 0.0 (.y Physics/gravity) 0.0) ; TODO: Planet gravity
                      inputs)
-        adjusted-diff (v3* (v3* diff speed) Time/deltaTime)]
+        adjusted-diff (v3* (v3* diff (::speed obj-state)) Time/deltaTime)]
     adjusted-diff))
 
-; TODO: Update ns name to move and call this fixed-update
-(defn move! [^GameObject obj]
-  (comment .. (cmpt obj CharacterController)
-      (Move (move (get-inputs!)))))
+(defn fixed-update! [^GameObject obj]
+  ; TODO: Apply diff
+  (let [diff (calculate-diff (state obj) (get-inputs!))]
+    ))
