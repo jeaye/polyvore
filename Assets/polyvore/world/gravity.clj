ae1324cb98a35ddae33f6af8f397fd2264ccb935
jeaye <contact@jeaye.com>
2017-03-14 21:38:42 -0700

    Add state to world gravity

--- Assets/polyvore/world/gravity.clj
+++ Assets/polyvore/world/gravity.clj
@@ -5,23 +5,26 @@
              [linear :refer :all]]
             [polyvore.util.lang :refer :all]))
 
-; TODO: state
-(def radius 110.0)
-(def mass 100000.0)
-(def G 1.0)
+(defn init-state! [^GameObject obj]
+  (doto obj
+    (set-state! ::radius 100.0)
+    (set-state! ::mass 100000.0)
+    (set-state! ::G 1.0)))
 
-(defn gravitation [mass-1 mass-2 distance]
+(defn gravitation [mass-1 mass-2 distance G]
   (* G (/ (* mass-1 mass-2)
           (* distance distance))))
 
 (defn fixed-update! [^GameObject world]
-  (let [world-transform (.transform world)
+  (let [world-state (state world)
+        world-transform (.transform world)
         world-position (.position world-transform)
-        affected (Physics/OverlapSphere world-position radius)]
+        affected (Physics/OverlapSphere world-position (::radius world-state))]
     (doseq [^Collider collider affected]
       (let [difference (v3- world-position (.. collider transform position))]
         (when-let [body (.attachedRigidbody collider)]
-          (let [g-force (gravitation mass (.mass body) (.magnitude difference))
+          (let [g-force (gravitation (::mass world-state) (.mass body)
+                                     (.magnitude difference) (::G world-state))
                 normal-force (-> (.normalized difference)
                                  (v3* g-force))]
             (.. body (AddForce (v3* normal-force Time/deltaTime)))))))))
