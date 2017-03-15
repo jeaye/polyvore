f16f061457e07e60738e2c57937835ef398fcf75
jeaye <contact@jeaye.com>
2017-03-14 22:34:47 -0700

    Add state to camera follow hook

--- Assets/polyvore/camera/follow.clj
+++ Assets/polyvore/camera/follow.clj
@@ -5,25 +5,33 @@
              [linear :refer :all]]
             [polyvore.util.lang :refer :all]))
 
-(def distance 10.0)
-(def height 3.0)
-(def damping 1.0)
-(def smooth-rotation true)
-(def follow-behind true)
-(def rotation-damping 1.0)
+(defn init-state! [^GameObject obj]
+  (doto obj
+    (set-state! ::target (.transform (object-named "player")))
+    (set-state! ::distance 10.0)
+    (set-state! ::height 3.0)
+    (set-state! ::translation-damping 1.0)
+    (set-state! ::rotation-damping 1.0)
+    (set-state! ::smooth-rotation? true)
+    (set-state! ::behind? true)))
 
 (defn fixed-update! [^GameObject obj]
-  (let [target (.transform (object-named "player")) ; TODO state
+  (let [obj-state (state obj)
+        target (::target obj-state)
         transform (.transform obj)
-        behind (if follow-behind -1.0 1.0)
-        wanted (.. target (TransformPoint 0.0 height (* behind distance)))]
+        behind (if (::behind? obj-state) -1.0 1.0)
+        wanted (.. target (TransformPoint 0.0
+                                          (::height obj-state)
+                                          (* behind (::distance obj-state))))]
     (update! (.position transform)
-             #(Vector3/Lerp % wanted (* Time/deltaTime damping)))
-    (if smooth-rotation
+             #(Vector3/Lerp % wanted (* Time/deltaTime
+                                        (::translation-damping obj-state))))
+    (if (::smooth-rotation? obj-state)
       (let [wanted-rotation (Quaternion/LookRotation (v3- (.position target)
                                                           (.position transform))
                                                      (.up target))]
         (update! (.rotation transform)
                  #(Quaternion/Slerp % wanted-rotation
-                                    (* Time/deltaTime rotation-damping)))))
+                                    (* Time/deltaTime
+                                       (::rotation-damping obj-state))))))
       (.. transform (LookAt target (.up target)))))
