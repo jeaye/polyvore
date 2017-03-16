c166cec9dd3401258e0d4b6816fabd579504d4f0
jeaye <contact@jeaye.com>
2017-03-15 23:20:09 -0700

    Better use the planet curve with the camera

--- Assets/polyvore/camera/follow.clj
+++ Assets/polyvore/camera/follow.clj
@@ -1,13 +1,16 @@
 (ns polyvore.camera.follow
-  (:import [UnityEngine GameObject Vector3 Quaternion Time])
+  (:import [UnityEngine GameObject Transform Vector3 Quaternion Time])
   (:require [arcadia
              [core :refer :all]
              [linear :refer :all]]
-            [polyvore.util.lang :refer :all]))
+            [polyvore.solar-system :as solar-system]
+            [polyvore.util
+             [lang :refer :all]
+             [arcadia :refer :all]]))
 
-(defn init-state! [^GameObject obj]
-  (doto obj
-    (set-state! ::target (.transform (object-named "player")))
+(defn init-state! [^GameObject camera]
+  (doto camera
+    (set-state! ::target (placeholder Transform))
     (set-state! ::distance 10.0)
     (set-state! ::height 3.0)
     (set-state! ::translation-damping 1.0)
@@ -15,23 +18,28 @@
     (set-state! ::smooth-rotation? true)
     (set-state! ::behind? true)))
 
-(defn fixed-update! [^GameObject obj]
-  (let [obj-state (state obj)
-        target (::target obj-state)
-        transform (.transform obj)
-        behind (if (::behind? obj-state) -1.0 1.0)
-        wanted (.. target (TransformPoint 0.0
-                                          (::height obj-state)
-                                          (* behind (::distance obj-state))))]
-    (update! (.position transform)
+; TODO: Refactor into helper functions
+(defn fixed-update! [^GameObject camera]
+  (let [camera-state (state camera)
+        target-transform (.transform (::target camera-state))
+        cam-transform (.transform camera)
+        planet-transform (.transform (solar-system/closest-planet target-transform))
+        up (v3- (.position target-transform) (.position planet-transform))
+        offset-direction (if (::behind? camera-state) -1.0 1.0)
+        offset (let [off-up (v3* (.normalized up) (::height camera-state))
+                     off-back (v3* (v3- (.forward target-transform))
+                                   (* offset-direction (::distance camera-state)))]
+                 (v3+ off-up off-back))
+        wanted (.. target-transform (TransformPoint offset))]
+    (update! (.position cam-transform)
              #(Vector3/Lerp % wanted (* Time/deltaTime
-                                        (::translation-damping obj-state))))
-    (if (::smooth-rotation? obj-state)
-      (let [wanted-rotation (Quaternion/LookRotation (v3- (.position target)
-                                                          (.position transform))
-                                                     (.up target))]
-        (update! (.rotation transform)
+                                        (::translation-damping camera-state))))
+    (if (::smooth-rotation? camera-state)
+      (let [wanted-rotation (Quaternion/LookRotation (v3- (.position target-transform)
+                                                          (.position cam-transform))
+                                                     up)]
+        (update! (.rotation cam-transform)
                  #(Quaternion/Slerp % wanted-rotation
                                     (* Time/deltaTime
-                                       (::rotation-damping obj-state))))))
-      (.. transform (LookAt target (.up target)))))
+                                       (::rotation-damping camera-state))))))
+      (.. cam-transform (LookAt target-transform up))))
