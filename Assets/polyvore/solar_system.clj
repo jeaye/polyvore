3ce93559a6f6ded45290d6ea8c0000ea9a0d493d
jeaye <contact@jeaye.com>
2017-03-15 21:56:49 -0700

    Add a way to find the closest planet

---
+++ Assets/polyvore/solar_system.clj
@@ -0,0 +1,29 @@
+(ns polyvore.solar-system
+  (:import [UnityEngine Transform GameObject Vector3])
+  (:require [arcadia
+             [core :refer :all]]
+            [polyvore.util
+             [lang :refer :all]
+             [arcadia :refer :all]]))
+
+(defn init-state! [^GameObject obj]
+  (doto obj
+    (set-state! ::planets {::home (placeholder GameObject)})))
+
+(def system-state (state (object-named "solar-system")))
+
+(defn closest-planet [^Transform transform]
+  ; TODO: optimize in one of two ways:
+  ;  1. cache closest planet in an atom
+  ;  2. convert position to quadrant and memoize lookup
+  (let [planets (::planets system-state)
+        position (.position transform)
+        distances (mapv #(vector (first %)
+                                 (Vector3/Distance position
+                                                   (.. (second %)
+                                                       transform
+                                                       position)))
+                        (seq planets))
+        sorted (sort-by second distances)
+        closest-key (ffirst sorted)]
+    (get planets closest-key)))
